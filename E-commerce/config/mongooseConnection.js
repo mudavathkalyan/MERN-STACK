35c9b38798ab8268765bd1bba5763c00cc3509c7
mudavathkalyan <kalyanmudavath238@gmail.com>
2024-11-01 21:13:43 +0530

    zabcart webApplication

---
+++ E-commerce/config/mongooseConnection.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+const dbgr=require('debug')("development:mongoose");
+
+const config=require("config");
+
+mongoose.connect(`${config.get("MONGODB_URI")}/Ecom`)
+.then(()=>{
+    dbgr("Connected");
+}).catch((err)=>{
+    dbgr(err);
+})
+
+module.exports=mongoose.connection;
---
+++ E-commerce/models/ownerModel.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+//owner who sells product
+const ownerSchema=mongoose.Schema({
+    fullname:{
+        type:String,
+        minLength:3,
+        trim:true,
+    },
+    email:String,
+    password:String,
+
+    product:{
+        type:Array,
+        default:[],
+    },
+
+    picture:String,
+    gstin:String,
+});
+
+module.exports=mongoose.model("owner",ownerSchema);
