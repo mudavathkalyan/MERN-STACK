9730582ab3fbbef3cbf08bb175535ed3f0a4f1fb
mudavathkalyan <kalyanmudavath238@gmail.com>
2024-12-03 02:21:49 +0530

    crated db

--- CHAT-APP/backend/src/controllers/auth.controller.js
+++ CHAT-APP/backend/src/controllers/auth.controller.js
@@ -1,11 +1,51 @@
-export const signup=(req,res)=>{
-   
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+
+export const signup=async (req,res)=>{
+     const {fullName,email,password}=req.body;
+   try{
+        if(password.length <6)
+        {
+            return res.status(400).json({message:"Password must be of atleast characters"});
+        }
+        const user=await User.findOne({email});
+        
+        if(user) return res.status(400).json({message:"User already exists"});
+        //if user not found
+        //hash passwors ans do signup
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=await bcrypt.hash(password,salt);
+
+        const newUser=new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            //generate jwt
+            generateToken(newUser._id,res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic,
+            })
+        }
+        else{
+            res.status(400).json({message:"Invalid user,Retry."});
+        }
+   }catch(error){
+        console.log("Error At Signup controller",error.message);
+   }
 };
 
 export const login=(req,res)=>{
-    res.send("Signup")
+    res.send("login")
 };
 
 export const logut=(req,res)=>{
-    res.send("Signup")
+    res.send("logout")
 };
--- CHAT-APP/backend/src/index.js
+++ CHAT-APP/backend/src/index.js
@@ -4,11 +4,14 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js" //.js because we are using type="module"
 
 import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
+
 dotenv.config()
 const PORT=process.env.PORT
 
-
 const app=express();
+app.use(express.json);//allows to extract json data of body
+
 
 
 app.use("/api/auth",authRoutes)
@@ -22,4 +25,5 @@ app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
     console.log("Runing at port:"+PORT);
+    connectDB();
 })
