903bf4bc3ff2b89c5e53de8e7a9d1089a1165898
mudavathkalyan <kalyanmudavath238@gmail.com>
2025-01-19 22:50:29 +0530

    project almost done

--- CHAT-APP/backend/src/index.js
+++ CHAT-APP/backend/src/index.js
@@ -2,34 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-// Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 5000; // Default to 5000 if PORT is undefined
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware setup
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve static files in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -38,8 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Start server
 server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
